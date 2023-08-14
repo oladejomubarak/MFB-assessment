2ff4a6d74493d5feba2ce3e3a64ac22803a7e4b3
oladejomubarak <oladejomubarakade@gmail.com>
2023-08-14 10:23:26 +0100

    committing chnages

--- src/home/components/HeroBody.js
+++ src/home/components/HeroBody.js
@@ -2,6 +2,13 @@ import React from "react";
 import Vector1 from '../../assets/images/Vector 1.png'
 import '../../../src/home/styles/HeroBody.css';
 import Vec from '../../assets/images/Vector.svg'
+import Elip1 from '../../assets/images/Ellipse 31.svg';
+import Elip2 from '../../assets/images/Ellipse 32.svg';
+import Elip3 from '../../assets/images/Ellipse 33.svg';
+import Elip4 from '../../assets/images/Ellipse 34.svg';
+import Elip5 from '../../assets/images/Ellipse 35.svg';
+import Elip6 from '../../assets/images/Ellipse 36.svg';
+import Elip7 from '../../assets/images/Ellipse 37.svg';
 const HeroBody = () => {
   return (  <div className="hero-body">
     <div className="vector1">
@@ -11,18 +18,31 @@ const HeroBody = () => {
       <p>No matter where you’re <br></br>going to, we’ll take you <br></br>there</p>
     </div>
     <div className="rectangle2">
-    <p>Where to?</p>
+    <p className="whereTo">Where to?</p>
     <div className="vertical-rectangle"></div>
     <p>Travel Type</p>
     <img src={Vec} className="vec"/>
-    <div className="vertical-rectangle"></div>
+    <div className="vertical-rectangle">
+
+    </div>
     <p>Duration</p>
     <img src={Vec} className="vec"/>
     <button>Submit</button>
     </div>
     <div className="elipses-people">
-      <div className="elipses"></div>
-      <div className="noOfPeople"></div>
+      <div className="elipses">
+        <img className="elipsee" src={Elip1}/>
+        <img className="elipse" src={Elip2}/>
+        <img className="elipse" src={Elip3}/>
+        <img className="elipse" src={Elip4}/>
+        <img className="elipse" src={Elip5}/>
+        <img className="elipse" src={Elip6}/>
+        <img className="elipse" src={Elip7}/>
+        <p className="elipse-content">+</p>
+      </div>
+      <div className="noOfPeople">
+        <p>2,500 people booked Tommorowland Event in last 24 hours</p>
+      </div>
     </div>
 
 
