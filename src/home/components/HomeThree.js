ae341dc9b1ddeb9822b3c53b529df78f0c8f0582
oladejomubarak <oladejomubarakade@gmail.com>
2023-08-15 11:25:03 +0100

    committing chnages

--- src/home/components/HomeThree.js
+++ src/home/components/HomeThree.js
@@ -9,15 +9,21 @@ const HomeThree = () => {
     <div className="home-three">
       <div className="home-three1">
       <div className="love-pics">
-        <p>Honeymoon Packages</p>
-        <img className="love2" src={Love2} alt=""/>
-        <img className="love1" src={Love1} alt=""/>
-        <img className="love3" src={Love3} alt=""/>
+        <div className="honeymoon-packages-div">
+        {/* <p>Honeymoon Packages</p> */}
+        </div>
         <div className="love4-div">
         <img className="love4" src={Love4} alt=""/>
-        </div>
+        </div> 
       </div>
-      <div className="honeymoon-packages"></div>
+      <div className="honeymoon-packages">
+        <p>Honeymoon Specials</p>
+        <p>Our Romantic Tropical Destinations</p>
+        <p>Et labore harum non nobis ipsum eum molestias mollitia et corporis praesentium a laudantium 
+          internos. Non quis eius quo eligendi corrupti et fugiat nulla qui soluta recusandae in maxime quasi aut 
+          ducimus illum aut optio quibusdam!</p>
+        <button>View Packages</button>
+      </div>
       </div>
       <div className="home-three2">
       <div className="pluses">
@@ -38,7 +44,9 @@ const HomeThree = () => {
         </div>       
       </div>
       </div>
-
+      <img className="love2" src={Love2} alt=""/>
+        <img className="love1" src={Love1} alt=""/>
+        <img className="love3" src={Love3} alt=""/>
     </div>
    );
 }
